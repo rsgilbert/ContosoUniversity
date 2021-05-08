bf4cafdd70b3c51166d1887b55a5fc63146fecdd
Gilboot <ssenyonjogilbert@gmail.com>
2021-05-08 07:04:55 +0300

    Ran ColumnFirstName migrations. Added Instructor and OfficeAssignment models.

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -2,16 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        // The first character should be uppercase and the remaining characters alphabetical
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")] 
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [StringLength(50, ErrorMessage ="First name can not be longer that 50 characters")]
+        [Column("FirstName")]
+        [Display(Name ="First Name")]
         public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
+        [Display(Name ="Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name ="Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
